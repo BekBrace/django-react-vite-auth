0d69724207f6018f99a80a7ab390abababf33517
Bek Brace <60483846+BekBrace@users.noreply.github.com>
2023-10-02 11:32:12 -0700

    Update settings.py
    Removing SECRET_KEY from Django Settings.py

--- djangocookieauth/settings.py
+++ djangocookieauth/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)uzh9907j5-c5s%05utt!e=m6xivuvu_1kzhp$y9mb92(4#0z9'
+SECRET_KEY = 'YOUR_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
